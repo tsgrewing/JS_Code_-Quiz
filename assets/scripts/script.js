d516f3c72c9c4e4087b888f629b6d569466aec1a
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-05-27 00:15:18 -0400

    set up files and folders

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+// set up a variable with an array of question objects(each index with a question, array of answers, and the correct answer that matches one of the answers in the array)
+
+// Variable for score
+
+// variable for remaining time
+
+// set variables for html elements
+
+// start quiz function (when startButton is clicked), hide start splash screen and show first question
+
+// add event listener for question div 
+
+// function to determine if correct answer was clicked, if it was award point(s), if not deduct 10 seconds from timer
+
+// function to hide
+
